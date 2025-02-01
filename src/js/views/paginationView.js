47998f2650bcba160306fbb6a2055c57d2a3ef6b
Sintayehu <sentflix@gmail.com>
2025-02-02 02:36:56 +0300

    update icon2

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -1,5 +1,5 @@
 import View from './View.js';
-import icons from '/img/icons.svg';
+import icons from 'url:../../img/icons.svg';
 class paginationView extends View {
   _parentElement = document.querySelector('.pagination');
   addHandlerClick(handler) {
--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -1,6 +1,6 @@
 import View from './View.js';
 import previewView from './previewView.js';
-import icons from '/img/icons.svg';
+import icons from 'url:../../img/icons.svg';
 class ResultsView extends View {
   _parentElement = document.querySelector('.results');
   _errorMessage = 'No recipe found for your query! Please try again ;)';
