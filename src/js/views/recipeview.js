8d5782f73226eb24c9f57d5ae6b32d2d46891446
Sintayehu <sentflix@gmail.com>
2025-02-05 03:34:49 +0300

    aaaaaa

--- src/js/views/recipeview.js
+++ src/js/views/recipeview.js
@@ -44,7 +44,7 @@ class RecipeView extends View {
     <div class="recipe__details">
       <div class="recipe__info">
         <svg class="recipe__info-icon">
-          <use href="${icons}#icon-clock"></use>
+          <use href="${icons}.svg#icon-clock"></use>
         </svg>
         <span class="recipe__info-data recipe__info-data--minutes">${
           this._data.cookingTime
@@ -53,7 +53,7 @@ class RecipeView extends View {
       </div>
       <div class="recipe__info">
         <svg class="recipe__info-icon">
-          <use href="${icons}#icon-users"></use>
+          <use href="${icons}.svg#icon-users"></use>
         </svg>
         <span class="recipe__info-data recipe__info-data--people">${
           this._data.servings
@@ -64,13 +64,13 @@ class RecipeView extends View {
           <button class="btn--tiny btn--update-servings" data-update-to="
           ${this._data.servings - 1}">
             <svg>
-              <use href="${icons}#icon-minus-circle"></use>
+              <use href="${icons}.svg#icon-minus-circle"></use>
             </svg>
           </button>
           <button class="btn--tiny btn--update-servings"data-update-to="
           ${this._data.servings + 1}">
             <svg>
-              <use href="${icons}#icon-plus-circle"></use>
+              <use href="${icons}.svg#icon-plus-circle"></use>
             </svg>
           </button>
         </div>
@@ -78,12 +78,12 @@ class RecipeView extends View {
 
       <div class="recipe__user-generated ${this._data.key ? '' : 'hidden'}">  
             <svg>
-              <use href="${icons}#icon-user"></use>
+              <use href="${icons}.svg#icon-user"></use>
             </svg>
           </div>
       <button class="btn--round btn--bookmark">
         <svg class="">
-          <use href="${icons}#icon-bookmark${
+          <use href="${icons}.svg#icon-bookmark${
       this._data.bookmarked ? '-fill' : ''
     }"></use>
         </svg>
@@ -111,7 +111,7 @@ class RecipeView extends View {
       }" target="_blank">
         <span>Directions</span>
         <svg class="search__icon">
-          <use href="${icons}#icon-arrow-right"></use>
+          <use href="${icons}.svg#icon-arrow-right"></use>
         </svg>
       </a>
     </div>`;
@@ -120,7 +120,7 @@ class RecipeView extends View {
   _generateMarkupIngredient(ing) {
     return `<li class="recipe__ingredient">
       <svg class="recipe__icon">
-        <use href="${icons}#icon-check"></use>
+        <use href="${icons}.svg#icon-check"></use>
       </svg>
       <div class="recipe__quantity">${
         ing.quantity ? new Fraction (ing.quantity).toString() : ''
